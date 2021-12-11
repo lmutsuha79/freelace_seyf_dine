8c51c90a33d2276341b4772a3aaa4e387a4b3176
Khelil Yasser Djamel Eddie <lmutsuha79@gmail.com>
2021-12-11 11:43:05 +0100

    add more resposivity to conatiners

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,24 @@ module.exports = {
       fontFamily: {
         mainFont: "Poppins"
       },
+      container: {
+        center: true,
+
+        // padding: {
+        //   // DEFAULT: '1rem',
+        //   sm: '0px 98px',
+        //   lg: '0px 98px',
+        //   xl: '0px 98px',
+        //   '2xl': '0px 98px',
+        // },
+      },
       colors: {
         white: "#FCFCFD",
         black: "#23262F",
         gray: "#777E90",
         light_ray: "#E6E8EC",
         blue: "#3772FF",
+        dark_blue: "#0045EA",
         border_color_green: "#00D3A1",
         border_color_gray: "#707070",
         green: "#45B26B"
