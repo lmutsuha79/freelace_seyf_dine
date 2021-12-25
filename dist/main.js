6579e45947cc4febb796540070a3652562705cdb
Khelil Yasser Djamel Eddie <lmutsuha79@gmail.com>
2021-12-25 10:55:32 +0100

    add few changes

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-// {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{    nav     }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+    // {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{    nav     }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
     var mb_nav = document.getElementById('mb_nav');
     var mb_nav_btn = document.getElementById('mb_nav_btn');
     var close_nav_btn = document.getElementById('close_nav_btn');
@@ -13,9 +13,9 @@
     mb_nav_btn.addEventListener('click',mb_nav_btn_click);
 
     function mb_nav_btn_click(){
+        mb_nav.classList.toggle('-translate-x-full')
         mb_nav.classList.toggle('translate-x-0');
         oppen_nav_btn.classList.toggle('hide_nav_btn');
         close_nav_btn.classList.toggle('hide_nav_btn');
     
     }
-// {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
